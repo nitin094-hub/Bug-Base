f5387767734e64bb53ffbd6aff4caaa04e967a15
shero4 <goyal.dhruva@gmail.com>
2021-12-29 13:12:16 +0530

    relative pathing

--- src/components/StatsTodayContainer.js
+++ src/components/StatsTodayContainer.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import 'D:/project web/bugbase project/your-project-name/src/style/StatsTodayContainer.css'
+import React from "react";
+import "../style/StatsTodayContainer.css";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import CircularProgressBar from './CircularProgressBar';
-
+import CircularProgressBar from "./CircularProgressBar";
 
 function StatsToday(props) {
-    // console.log(props.Percent)
-    return (
-        <div className='statsTodaycontainer'>
-            <div className="container-head">
-                <h3>{props.title}</h3>
-                <BsThreeDotsVertical style={{cursor:"pointer"}} size={23} />
-            </div>
-            <div className="data-container">
-                <CircularProgressBar rotate="18" color={`${props.color}`} rotatePercent={`${props.percent}`} />
-                <div className="data">
-                    <h2>{props.number}</h2>
-                    <p>{props.title} Today</p>
-                </div>
-            </div>
+  // console.log(props.Percent)
+  return (
+    <div className="statsTodaycontainer">
+      <div className="container-head">
+        <h3>{props.title}</h3>
+        <BsThreeDotsVertical style={{ cursor: "pointer" }} size={23} />
+      </div>
+      <div className="data-container">
+        <CircularProgressBar
+          rotate="18"
+          color={`${props.color}`}
+          rotatePercent={`${props.percent}`}
+        />
+        <div className="data">
+          <h2>{props.number}</h2>
+          <p>{props.title} Today</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default StatsToday
+export default StatsToday;
--- src/components/TeamMemberData.js
+++ src/components/TeamMemberData.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import 'D:/project web/bugbase project/your-project-name/src/style/TeamMemberData.css'
+import React from "react";
+import "../style/TeamMemberData.css";
 
 function TeamMemeberData(props) {
-    return (
-        <div className='TeamMemberDataContainer'>
-            <div className="image">
-                <img src={props.image} alt="" />
-                <div className='MemberData'>
-
-                    <h3>{props.name}</h3>
-                    <p style={{color:"grey",fontSize:"13px"}}>{props.designation}</p>
-                </div>
-            </div>
-            <div style={{marginTop:"10px",flexBasis:"39px"}}>
-                <button className='btn'>{props.performance}</button>
-            </div>
+  return (
+    <div className="TeamMemberDataContainer">
+      <div className="image">
+        <img src={props.image} alt="" />
+        <div className="MemberData">
+          <h3>{props.name}</h3>
+          <p style={{ color: "grey", fontSize: "13px" }}>{props.designation}</p>
         </div>
-    )
+      </div>
+      <div style={{ marginTop: "10px", flexBasis: "39px" }}>
+        <button className="btn">{props.performance}</button>
+      </div>
+    </div>
+  );
 }
 
-export default TeamMemeberData
+export default TeamMemeberData;
